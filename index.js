2cfe5f05b5a95c4b655c7e2c7f7f19340b2b95cd
DLeaguer <immostlikely@hotmail.com>
2018-12-11 11:51:01 -1000

    group raw json into category by film title, add CSS styles

--- index.js
+++ index.js
@@ -2,19 +2,42 @@ console.log('script attached')
 
 $('.btn').click(function() {
   
-  $('.text').text('loading . . .');
+  $('.loading').text('loading . . .');
   
   $.ajax({
     method: "GET",
     url: "https://swapi.co/api/films/",
     dataType: 'json',
-    success: buildHtmlElements,
+    success: getFilmData,
+    error: onError
   });
   
 });
 
 function buildHtmlElements(responseData) {
-  $('.text').text(JSON.stringify(responseData));
+
 }
 
-getFilmData(id)
+function getFilmData(id) {
+  console.log('id:\n',id)
+  $('.loading').html('');
+  for (var i=0; i<id.results.length; i++) {
+    let title = id.results[i].title;
+    let episode = id.results[i].episode_id;
+    let director = id.results[i].director;
+    let producer = id.results[i].producer;
+    let openCrawl = id.results[i].opening_crawl;
+    let releaseDate = id.results[i].release_date;
+    $('#films').append('<h2>'+title+'</h2>');
+    $('#films').append('<p>'+'episode: '+episode+'</p>');
+    $('#films').append('<p>'+'director: '+director+'</p>');
+    $('#films').append('<p>'+'producer(s): '+producer+'</p>');
+    $('#films').append('<p>'+'<span style="color:blue">'+'opening crawl: '+'<br>'+'</span>'+openCrawl+'</p>');
+    $('#films').append('<p>'+'release date: '+releaseDate+'</p>');
+    $('#films').append('<br><hr><br>')
+  }
+}
+
+function onError() {
+  $('#films').html('Error 503 could not load data')
+}
